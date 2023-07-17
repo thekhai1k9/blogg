fb7a3dd29e525fd38efede63715b749fcb1a3f18
thekhai1k9 <thekhai1k9@gmail.com>
2023-07-17 17:30:27 +0700

    commit code

--- api/src/controllers/post/CreatePost.ts
+++ api/src/controllers/post/CreatePost.ts
@@ -1,13 +1,13 @@
-import { Response, Request } from "express";
-import Post from "../../models/post";
-import multer, { Multer } from "multer";
-import { storage } from "../../middleware/Uploadfile";
+import { Response, Request } from "express"
+import Post from "../../models/post"
+import multer, { Multer } from "multer"
+import { storage } from "../../middleware/Uploadfile"
 
 const upload: Multer = multer({ storage: storage })
 const CreatePost = async (req: Request, res: Response) => {
   // Cấu hình middleware tải lên của multer
   try {
-    const { title, user_id, desc, content, type_post, date, image } = req.body;
+    const { title, user_id, desc, content, type_post, date, image } = req.body
     if (req.body.image) {
       const post = await Post.create({
         user_id,
@@ -16,23 +16,23 @@ const CreatePost = async (req: Request, res: Response) => {
         image,
         content,
         type_post,
-        date,
-      });
+        date
+      })
       return res.status(200).json({
         data: post,
-        message: "Tạo mới bài viết thành công",
-      });
+        message: "Tạo mới bài viết thành công"
+      })
     } else {
       return res.status(400).json({
-        message: "Tệp hình ảnh không hợp lệ",
-      });
+        message: "Tệp hình ảnh không hợp lệ"
+      })
     }
   } catch (error) {
-    console.error("Lỗi khi tạo bài viết:", error);
+    console.error("Lỗi khi tạo bài viết:", error)
     return res.status(500).json({
-      message: "Có lỗi xảy ra từ server",
-    });
+      message: "Có lỗi xảy ra từ server"
+    })
   }
-};
+}
 
-export default [upload.single("image"), CreatePost];
+export default [upload.single("image"), CreatePost]
